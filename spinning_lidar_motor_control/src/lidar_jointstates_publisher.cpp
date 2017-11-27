981019b5a289284d72dba543efdb2b71384d8b9d
YoshuaNava <yoshua.nava.chocron@gmail.com>
2017-11-28 00:35:40 +0100

    Fixed a small bug when calling joint state publisher. Removed tf broadcasting, it's better to use robot state publisher

--- spinning_lidar_motor_control/src/lidar_jointstates_publisher.cpp
+++ spinning_lidar_motor_control/src/lidar_jointstates_publisher.cpp
@@ -1,5 +1,4 @@
 #include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
 #include <sensor_msgs/JointState.h>
 #include "spinning_lidar_motor_control/MotorState.h"
 
@@ -7,6 +6,7 @@
 std::string motor_state_topic;
 std::string lidar_platform_link;
 std::string lidar_axis_link;
+std::string spinning_lidar_joint;
 double startup_delay;
 
 ros::Publisher spinning_lidar_joint_pub;
@@ -16,16 +16,11 @@ void motorStateCallback(const spinning_lidar_motor_control::MotorState::ConstPtr
 {
     double angle = -msg->angle;
     double vel = -msg->curr_vel;
-    static tf::TransformBroadcaster br;
-    tf::Transform transform;
-    transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
-    tf::Quaternion q;
-    q.setRPY(angle, 0.0, 0.0);
-    transform.setRotation(q);
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), lidar_platform_link, lidar_axis_link));
 
     sensor_msgs::JointState joint_state_msg;
     joint_state_msg.header.stamp = ros::Time::now();
+    joint_state_msg.header.frame_id = lidar_platform_link;
+    joint_state_msg.name.push_back(spinning_lidar_joint);
     joint_state_msg.position.push_back(angle);
     joint_state_msg.velocity.push_back(vel);
 
@@ -35,12 +30,13 @@ void motorStateCallback(const spinning_lidar_motor_control::MotorState::ConstPtr
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "lidar_tf_publisher_node");
+	ros::init(argc, argv, "lidar_jointstates_publisher_node");
 	ros::NodeHandle nh;
 
     nh.param("motor_state_topic", motor_state_topic, std::string("/spinning_lidar/motor_state"));
     nh.param("lidar_platform_link", lidar_platform_link, std::string("laser_mount"));
     nh.param("lidar_axis_link", lidar_axis_link, std::string("laser_axis"));
+    nh.param("spinning_lidar_joint", spinning_lidar_joint, std::string("lidar_spin_joint"));
     nh.param("startup_delay", startup_delay, 3.0);
 
     spinning_lidar_joint_pub = nh.advertise<sensor_msgs::JointState>("spinning_lidar/joint_states", 1);
@@ -48,7 +44,6 @@ int main(int argc, char** argv)
 
     ros::Duration(startup_delay).sleep();
     ROS_INFO("Publishing joint state of the rotating platform");
-    ROS_INFO("Broadcasting TF from the rotating platform to the lidar frame");
     ros::spin();
 
     return EXIT_SUCCESS;
