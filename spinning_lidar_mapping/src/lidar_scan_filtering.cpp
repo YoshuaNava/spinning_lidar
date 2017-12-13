e59ecd995d691d983dc88f991e4b55d3f28810c1
YoshuaNava <yoshua.nava.chocron@gmail.com>
2017-12-13 21:30:50 +0100

    Implemented the feature extraction method from LOAM

--- spinning_lidar_mapping/src/lidar_scan_filtering.cpp
+++ spinning_lidar_mapping/src/lidar_scan_filtering.cpp
@@ -3,17 +3,26 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
 #include <tf/transform_listener.h>
 #include <tf/message_filter.h>
 #include <message_filters/subscriber.h>
 #include <laser_geometry/laser_geometry.h>
 
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+
 
 float inf = std::numeric_limits<float>::infinity();
-ros::Publisher filtered_scan_pub;
 int num_points_thesh = 5000;
-std::string laser_scan_topic, filtered_scan_topic, laser_link;
 double min_dist_to_sensor;
+bool apply_voxel_filter;
+
+std::string laser_scan_topic, filtered_scan_topic, filtered_cloud_topic, laser_link;
+ros::Publisher filtered_scan_pub;
 
 
 class DynamicLaserToPointCloud
@@ -22,7 +31,7 @@ public:
 	ros::NodeHandle nh_;
 	message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub_;
 
-	DynamicLaserToPointCloud(ros::NodeHandle nh, std::string laser_scan_topic, std::string laser_link, std::string filtered_scan_topic, double min_dist_to_sensor) :
+	DynamicLaserToPointCloud(ros::NodeHandle nh, std::string laser_scan_topic, std::string laser_link, std::string filtered_scan_topic, std::string filtered_cloud_topic, double min_dist_to_sensor) :
 		nh_(nh),
 		laser_sub_(nh_, laser_scan_topic, 10),
 		tf_laser_filter_(laser_sub_, tf_listener_, laser_link, 10)
@@ -30,16 +39,18 @@ public:
 		laser_scan_topic_ = laser_scan_topic;
 		laser_link_ = laser_link;
 		filtered_scan_topic_ = filtered_scan_topic;
+		filtered_cloud_topic_ = filtered_cloud_topic;
 		tf_laser_filter_.setTolerance(ros::Duration(tf_filter_tol_));
 		tf_laser_filter_.registerCallback( boost::bind(&DynamicLaserToPointCloud::scanCallback, this, _1) );
 		filtered_scan_pub_ = nh_.advertise<sensor_msgs::LaserScan>(filtered_scan_topic, 1);
+		filtered_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>(filtered_cloud_topic_, 1);
 		min_dist_to_sensor_ = min_dist_to_sensor;
 	}
 
 private:
 	
-	ros::Publisher filtered_scan_pub_;
-	std::string laser_scan_topic_, laser_link_, filtered_scan_topic_;
+	ros::Publisher filtered_scan_pub_, filtered_cloud_pub_;
+	std::string laser_link_, laser_scan_topic_, filtered_scan_topic_, filtered_cloud_topic_;
 	tf::TransformListener tf_listener_;
 	tf::MessageFilter<sensor_msgs::LaserScan> tf_laser_filter_;
 	laser_geometry::LaserProjection laser_projector_;
@@ -50,6 +61,7 @@ private:
 	{
 		sensor_msgs::LaserScan filtered_scan; //create new LaserScan msg for filtered points
 
+		// LIDAR scan filtering
 		// Reference: https://github.com/RobustFieldAutonomyLab/spin_hokuyo/blob/master/src/hokuyo_robot_filter.cpp
 		int num_range_meas = scan->ranges.size();
 		int num_int_meas = scan->intensities.size();
@@ -63,7 +75,6 @@ private:
 		filtered_scan.range_max = inf;
 		filtered_scan.intensities[num_int_meas];
 		filtered_scan.ranges.resize(num_range_meas);
-
 		for(int n=0; n<num_range_meas ;n++)
 		{
 			if (scan->ranges[n] > min_dist_to_sensor_)
@@ -76,8 +87,21 @@ private:
 				filtered_scan.ranges[n] = inf;
 			}
 		}
-
 		filtered_scan_pub_.publish(filtered_scan);
+
+		// Projection of laser scans into point clouds
+    	sensor_msgs::PointCloud2 cloud;
+		try
+		{
+			laser_projector_.transformLaserScanToPointCloud(laser_link_, filtered_scan, cloud, tf_listener_);
+		}
+		catch (tf::TransformException& e)
+		{
+			std::cout << e.what();
+			return;
+		}
+
+		filtered_cloud_pub_.publish(cloud);
 	}
 };
 
@@ -93,9 +117,11 @@ int main(int argc, char** argv)
 	nh.param("laser_link", laser_link, std::string("laser"));
 	nh.param("laser_scan_topic", laser_scan_topic, std::string("spinning_lidar/scan"));
 	nh.param("filtered_scan_topic", filtered_scan_topic, std::string("spinning_lidar/filtered_scan"));
+	nh.param("filtered_cloud_topic", filtered_cloud_topic, std::string("spinning_lidar/filtered_cloud"));
+	nh.param("apply_voxel_filter", apply_voxel_filter, false);
 
 	ROS_INFO("Dynamic (using TF) filtering of laser scans");
-	DynamicLaserToPointCloud laser_pcl_converter(nh, laser_scan_topic, laser_link, filtered_scan_topic, min_dist_to_sensor);
+	DynamicLaserToPointCloud laser_pcl_converter(nh, laser_scan_topic, laser_link, filtered_scan_topic, filtered_cloud_topic, min_dist_to_sensor);
 		
 	ros::spin();
 
